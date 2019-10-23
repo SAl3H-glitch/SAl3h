9fa562741e9e5fcb413b7c98d2e9effdfe075996
swl3h <45856869+swl3h@users.noreply.github.com>
2019-10-23 22:38:38 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<HTML>
+<HEAD>
+    <span><a href="https://www.sex.com" >
+
+<img src="https://www.sex.com/images/sex/logo.png">
+    </a></span>
+
+        <a class="active" href="#home" style="padding-bottom: 20px; background-color: red;font-size:xx-large;padding-top: 10px;padding-left: 20px;font-family: sans-serif;padding-right: 20px; " >Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>t
+        <input type="text" placeholder="Search..">
+    <button onclick="">submi</button>
+
+</HEAD>
+<BODY>
+
+</BODY>
+</HTML>
